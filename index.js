341a0df3aac26ccce0ff82cb34bb7ab60201847a
t-wilkinson <winston.trey.wilkinson@gmail.com>
2020-09-08 20:19:15 -0400

    updates

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ export const async$ = new rxjs.Subject()
 export const action$ = new rxjs.Subject()
 export const dispatch = (action) => action$.next(action)
 
+// const mergeReducers = (reducerEntries) => (oldState, action) => {
+//   let state = {...oldState}
+//     let entries = Object.entries(reducerEntries)
+//     if (action.filter) {
+//       try { // too many potential errors to check. Move on if error
+//         reducerEntries[action.filter][action.type](state[action.filter], action)
+//       } catch(error) { }
+//     }
+//     else {
+//       for (let [, reducers] of entries)
+//         if (typeof reducers[action.type] === "function")
+//           reducers[action.type](state, action)
+//     }
+//   // let javascript reflow before triggering async epics
+//   setTimeout(() => async$.next([state, action]), 0)
+//   return state
+// }
+
 const mergeReducers = (reducerEntries) => (oldState, action) => {
   let newState = produce(oldState, (state) => {
     let entries = Object.entries(reducerEntries)
@@ -22,29 +40,25 @@ const mergeReducers = (reducerEntries) => (oldState, action) => {
     }
   })
   // let javascript reflow before triggering async epics
-  setTimeout(() => async$.next(newState, action), 0)
+  setTimeout(() => async$.next([newState, action]), 0)
   return newState
 }
-
-export const ofMap = (filters, types, f) =>
-  ops.map((state, action) => {
-    if ((types.length === 0 || types.includes(action.type) || !action.type) &&
-        (filters.length === 0 || filters.includes(action.filter) || !action.filter))
-      return f([state, action]) || null
-    return null
-  })
 
 export const createStore = (slice) => {
   let slices = { reducers: {}, states: {}, actions: {} }
   for (let [name, {initialState, reducers}] of Object.entries(slice)) {
+    // init state
     if (typeof initialState === "function")
       slices.states[name] = initialState()
     else slices.states[name] = initialState
 
+    // add reducer
     slices.reducers[name] = reducers
+
+    // create action function
     slices.actions[name] = {}
     for (let [rname] of Object.entries(reducers)) {
-      slices.actions[name][rname] = (action) => dispatch(({filter: name, type: rname, ...action}))
+      slices.actions[name][rname] = (action={}) => dispatch(({filter: name, type: rname, ...action}))
     }
   }
   slices.reducers = mergeReducers(slices.reducers)
@@ -61,5 +75,16 @@ export const createStore = (slice) => {
   return [Provider, useStore, subscribe, slices.actions]
 }
 
+// Listen to '$action.next()' and pipe stream to function
 export const epic = (...fs) => rxjs.pipe(...fs)(async$).subscribe(v => v && action$.next(v))
 export const mergeEpics = (...epics) => async$.pipe(ops.merge(...epics))
+
+export const ofMap = (filters, types, f) =>
+  epic(
+    ops.map(([state, action]) => {
+      if ((types.length === 0 || types.includes(action.type) || !action.type) &&
+        (filters.length === 0 || filters.includes(action.filter) || !action.filter))
+        return f(state, action) || null
+      return null
+    })
+  )
